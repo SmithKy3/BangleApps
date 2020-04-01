811e08292ee5f484a9de75decc814f0dc04c196d
Kye Smith <1250smit@gmail.com>
2020-04-01 16:24:07 +0100

    Fixerino fixerooed

--- apps/hexclock/app.js
+++ apps/hexclock/app.js
@@ -1,5 +1,6 @@
-const LCDWidth = g.getWidth();
-const LCDHeight = g.getHeight();
+const W = g.getWidth();
+const H = g.getHeight();
+const buf = Graphics.createArrayBuffer(W, 200, 1,{msb:true});
 
 class Vec3 {
   constructor(x, y, z) {
@@ -7,17 +8,17 @@ class Vec3 {
     this.y = y;
     this.z = z;
   }
-
+  clone() {
+    return new Vec3(this.x, this.y, this.z);
+  }
   originise() {
-    this.x -= LCDWidth*0.5;
-    this.y -= LCDHeight*0.5;
+    this.x -= W*0.25;
+    this.y -= H*0.25;
   }
-
   deOriginise() {
-    this.x += LCDWidth*0.5;
-    this.y += LCDHeight*0.5;
+    this.x += W*0.25;
+    this.y += H*0.25;
   }
-
   rotateX(deg) {
     const rad = deg * Math.PI / 180;
     const sin = Math.sin(rad);
@@ -30,7 +31,6 @@ class Vec3 {
     this.z = Y * sin + Z * cos;
     this.deOriginise();
   }
-
   rotateY(deg) {
     const rad = deg * Math.PI / 180;
     const sin = Math.sin(rad);
@@ -43,7 +43,6 @@ class Vec3 {
     this.z = Z * cos - X * sin;
     this.deOriginise();
   }
-
   rotateZ(deg) {
     const rad = deg * Math.PI / 180;
     const sin = Math.sin(rad);
@@ -56,15 +55,11 @@ class Vec3 {
     this.y = X * sin + Y * cos;
     this.deOriginise();
   }
-
-  clone() {
-    return new Vec3(this.x, this.y, this.z);
-  }
 }
 
-class Hexagon {
+class RotatingHexagon {
   constructor() {
-    this.vertices = [new Vec3(LCDWidth *0.5, LCDHeight*0.1, 0)];
+    this.vertices = [new Vec3(W *0.25, 0, 0)];
 
     for (let i=1; i<6; i++) {
       const next = this.vertices[i-1].clone();
@@ -72,48 +67,67 @@ class Hexagon {
       this.vertices.push(next);
     }
   }
-
-  draw() {
-    g.clear();
-    g.setColor(255, 0, 255);
+  drawFrame() {
     const start = this.vertices[0];
-    g.moveTo(start.x, start.y);
+    buf.moveTo(start.x, start.y);
 
     for (let i=1; i<6; i++) {
       let vert = this.vertices[i];
-      g.lineTo(vert.x, vert.y);
+      buf.lineTo(vert.x, vert.y);
     }
 
-    g.lineTo(start.x, start.y);
-  }
+    buf.lineTo(start.x, start.y);
 
+    this.rotateX(5);
+    this.rotateY(5);
+    this.rotateZ(5);
+  }
   rotateX(deg) {
     this.vertices.forEach(vert => vert.rotateX(deg));
   }
-
   rotateY(deg) {
     this.vertices.forEach(vert => vert.rotateY(deg));
   }
-
   rotateZ(deg) {
     this.vertices.forEach(vert => vert.rotateZ(deg));
   }
 }
 
-
-function draw() {
-  const hex = new Hexagon();
-
-  function frame() {
-    hex.draw();
-    hex.rotateZ(10);
-  }
-
-  setInterval(frame, 100);
+function drawTime() {
+  const today = new Date();
+  let minutes = today.getMinutes().toString();
+  minutes = minutes.length === 1 ? '0'+minutes : minutes;
+  let seconds = today.getSeconds().toString();
+  seconds = seconds.length === 1 ? '0'+seconds : seconds;
+  const time = today.getHours() + ':' + minutes + ':' + seconds;
+  const date = today.getDate() + ' / ' + today.getMonth() + ' / ' + today.getFullYear();
+  buf.setFont('6x8', 5);
+  buf.setFontAlign(0); // centers text
+  buf.drawString(time, W*0.5, H*0.5, true);
+  buf.setFont('Vector12', 15);
+  buf.drawString(date, W*0.5, H*0.65, true);
 }
 
-Bangle.on('lcdPower', function(on) {
-  if (on) draw();
+const hex = new RotatingHexagon();
+function frame() {
+  buf.clear();
+  g.setColor(0.5, 1, 0.75);
+  hex.drawFrame();
+  drawTime();
+  g.drawImage({width:W,height:H,buffer:buf.buffer},0,40);
+}
+
+Bangle.on('lcdPower',function(on) {
+  if (on)
+    frame();
 });
 
+g.clear();
+Bangle.loadWidgets();
+Bangle.drawWidgets();
+// Update time once a second
+setInterval(frame, 1000);
+frame();
+
+// Show launcher when middle button pressed
 setWatch(Bangle.showLauncher, BTN2, {repeat:false,edge:"falling"});
